08528671805e8ebfd84816c70f34c772fd386efa
Neversoon <igorpapa326@gmail.com>
2025-07-11 23:22:28 +0300

    Added bomb count color

--- Assets/Scripts/Cell/CellView.cs
+++ Assets/Scripts/Cell/CellView.cs
@@ -46,9 +46,43 @@ public class CellView : MonoBehaviour
             bombCount.text = "";
             return;
         }
-
+        ChangeColorTextByCount(count);
+        
         bombCount.gameObject.SetActive(true);
 
         bombCount.text = $"{count}";
+    }
+    void ChangeColorTextByCount(int count)
+    {
+        switch (count)
+        {
+            case 1:
+                bombCount.color = Color.blue;
+                break;
+            case 2:
+                bombCount.color = Color.green;
+                break;
+            case 3:
+                bombCount.color = Color.red;
+                break;
+            case 4:
+                bombCount.color = new Color(0.5f, 0, 130f / 255f);
+                break;
+            case 5:
+                bombCount.color = new Color(129f / 255f, 1f / 255f, 2f / 255f);
+                break;
+            case 6:
+                bombCount.color = new Color(0.0f, 128f / 255f, 129 / 255f);
+                break;
+            case 7: 
+                bombCount.color = Color.black;
+                break;
+            case 8:
+                bombCount.color = new Color(128f / 255f, 128f / 255f, 128f / 255f);
+                break;
+            default:
+                bombCount.color = Color.black;
+                break;
+        }
     }
 }
